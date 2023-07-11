94bc3d45c12240204032a59b875aea74715637be
hyeon <skarndgus8899@gmail.com>
2023-07-11 17:48:55 +0900

    앱바 수정

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          elevation: 15,
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          title: Text(
+            "1명 없어서 슬프조를 소개합니다",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            )
+          ],
+        ),
       ),
     );
   }
