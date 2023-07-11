c1e53e7b85619d63fdd49b9b1a244aef39e97796
hyeon <skarndgus8899@gmail.com>
2023-07-11 20:09:10 +0900

    image test

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 15,
-          centerTitle: true,
-          backgroundColor: Colors.red,
-          title: Text(
-            "1명 없어서 슬프조를 소개합니다",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+      home: TeamShot(),
+    );
+  }
+}
+
+class TeamShot extends StatelessWidget {
+  const TeamShot({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 테스트 이미지 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "test": "test1",
+        "imgUrl":
+            "https://github.com/sparta-8Team/Team-Introducation/assets/101164731/5b6a14fd-e090-461c-899a-063800fe950f",
+      },
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 15,
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        title: Text(
+          "1명 없어서 슬프조를 소개합니다",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Divider(height: 1),
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                String category = dataList[index]['test'];
+                String imgUrl = dataList[index]['imgUrl'];
+                return Card(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.network(
+                        imgUrl,
+                        width: double.infinity,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      Text(
+                        category,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
-            )
-          ],
-        ),
+        ],
       ),
     );
   }
