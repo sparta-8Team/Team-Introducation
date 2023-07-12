f68667981a0a52173e50315b031c91046fe4c7ea
mwkimm <94061061+mwkimm@users.noreply.github.com>
2023-07-12 10:28:34 +0900

    picture 4개 붙인 코드

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,27 @@ class TeamShot extends StatelessWidget {
 // 테스트 이미지 데이터
     List<Map<String, dynamic>> dataList = [
       {
-        "test": "test1",
+        "test": "남궁현",
         "imgUrl":
             "https://github.com/sparta-8Team/Team-Introducation/assets/101164731/5b6a14fd-e090-461c-899a-063800fe950f",
       },
+      {
+        "test": "김민우",
+        "imgUrl":
+            "https://github.com/sparta-8Team/Team-Introducation/assets/94061061/ea96d517-36ac-4e26-8f73-24ee3b8a3450",
+      },
+      {
+        "test": "이성진님 이슈에 사진 다시올려주시면 감사하겠습니다.",
+        "imgUrl":
+            "https://github.com/sparta-8Team/Team-Introducation/assets/94061061/97e79ad3-d971-4a14-9827-f39cde82df66",
+      },
+      {
+        "test": "이동규님 이슈에 사진 다시올려주시면 감사하겠습니다.",
+        "imgUrl":
+            "https://github-production-user-asset-6210df.s3.amazonaws.com/94061061/252827449-357c96cf-3fe5-40e7-8986-47ab5a94ecad.png",
+      },
     ];
+
     return Scaffold(
       appBar: AppBar(
         elevation: 15,
@@ -51,7 +67,7 @@ class TeamShot extends StatelessWidget {
               );
             },
             icon: Icon(Icons.add),
-          )
+          ),
         ],
       ),
       body: Column(
